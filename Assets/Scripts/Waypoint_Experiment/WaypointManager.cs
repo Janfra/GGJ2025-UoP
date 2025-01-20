147754b828ab99047dbf869c82c6c2017edeca6a
Janfra <janfrancoherrera@gmail.com>
2025-01-20 14:38:19 +0000

    Project
    Initial Project

---
+++ Assets/Scripts/Waypoint_Experiment/WaypointManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    [SerializeField]
+    private Waypoint[] waypoints = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
